53b5a29110a97a83578936792c4251ec07e58b32
Aditya kumar <adityakmr9672@gmail.com>
2020-08-23 08:08:20 +0530

    saving image

--- App.tsx
+++ App.tsx
@@ -1,21 +1,94 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { Feather as Icon } from "@expo/vector-icons";
+import { Camera } from "expo-camera";
+import * as MediaLibrary from "expo-media-library";
+import * as Permissions from "expo-permissions";
+import React, { useEffect, useRef, useState } from "react";
+import { Text, TouchableOpacity, View } from "react-native";
 export default function App() {
+  const [hasPermission, setHasPermission] = useState<boolean | null>(null);
+  const [type, setType] = useState(Camera.Constants.Type.back);
+
+  const cam = useRef<Camera | null>();
+
+  const _takePicture = async () => {
+    if (cam.current) {
+      const options = { quality: 0.5, base64: true, skipProcessing: true };
+      let photo = await cam.current.takePictureAsync(options);
+
+      console.log(cam.current.getSupportedRatiosAsync());
+      const source = photo.uri;
+
+      if (source) {
+        handleSave(source);
+      }
+    }
+  };
+
+  const handleSave = async (photo: string) => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+    if (status === "granted") {
+      const assert = await MediaLibrary.createAssetAsync(photo);
+      await MediaLibrary.createAlbumAsync("Tutorial", assert);
+    } else {
+      console.log("Oh You Missed to give permission");
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <Camera ref={cam} style={{ flex: 1 }} type={type}>
+        <View
+          style={{
+            flex: 1,
+            backgroundColor: "transparent",
+            flexDirection: "row",
+          }}
+        >
+          <View
+            style={{
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "flex-end",
+            }}
+          >
+            <View>
+              <TouchableOpacity onPress={() => _takePicture()}>
+                <Icon name="aperture" size={50} color="white" />
+              </TouchableOpacity>
+            </View>
+            <View>
+              <TouchableOpacity
+                onPress={() => {
+                  setType(
+                    type === Camera.Constants.Type.back
+                      ? Camera.Constants.Type.front
+                      : Camera.Constants.Type.back
+                  );
+                }}
+              >
+                <Text
+                  style={{ fontSize: 18, marginBottom: 10, color: "white" }}
+                >
+                  Flip
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Camera>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
